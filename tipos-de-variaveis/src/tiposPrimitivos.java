108998ce90e5259f0afe3434bb1969a5b4a1296f
Luan Meneses <luan.meneses25@gmail.com>
2024-07-24 23:21:06 -0300

    operadores

---
+++ tipos-de-variaveis/src/tiposPrimitivos.java
@@ -0,0 +1,24 @@
+public class tiposPrimitivos {
+    public static void main(String[] args) throws Exception {
+        //Números Reais
+        float PI = 3.14F;
+        double salarioMinimo = 1400;
+
+        //Números Inteiros
+        byte digitosEmIII = 127;
+        short digitosEmV = 32767;
+        int digitosEmX = 2147483647;
+        long muitosDigitos = 9223372036854775807L;
+
+        //Literais
+        String nome = "luan";
+        char letra = 'L';
+
+        //Logico
+        boolean aprendendo = true;
+        boolean desistir = false;
+
+        System.out.println(aprendendo);
+        
+    }
+}
